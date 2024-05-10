bcf1107fe44464a4c629fe8a06fe32c93aa64fdb
AbelHaro <abelh2003@gmail.com>
2024-05-10 13:22:34 +0200

    fallo

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ REM Change directory to the location of the Dockerfile
 cd ./api
 
 REM Build Docker image
-docker build -t restapp .
+docker build -t server_rfid .
 
 REM Run Docker container
-docker run --rm -it -v database:/home/database -p 8888:5000 --name restapp restapp
+docker run --rm -it -v database:/home/database -p 8888:5000 --name server_rfid server_rfid
